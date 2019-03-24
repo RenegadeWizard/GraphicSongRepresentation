3f082cdfa9aa755a58d4e1fd958d22a409e72d04
Krzysztof Sychla <ksychla5@gmail.com>
2019-03-24 15:52:47 +0100

    logo

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,10 @@
 package sample;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
 
 public class Controller {
     public Button refresh;
